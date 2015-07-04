f6af372dafe5ba2d1d2b0cba29aefc72db333fc2
tamcy <tamcy@users.noreply.github.com>
2015-07-04 11:48:11 +0800

    add twig output adapter

---
+++ Exporter/Output/TwigTemplateOutputAdapter.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Sparkson\DataExporterBundle\Exporter\Output;
+
+use Sparkson\DataExporterBundle\Exporter\Column\Column;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Twig output adapter.
+ *
+ * This adapter accepts a `template` option, which defaults to `Resources/view/exporter/template.html`
+ * to render the data set in an HTML table. For performance reason the exported data will be stored in
+ * a temporary variable, and the template rendering is only called once when end() is called.
+ *
+ * @author Tamcy <tamcyhk@outlook.com>
+ */
+class TwigTemplateOutputAdapter extends BaseFlattenOutputAdapter
+{
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * @var null|array Header data
+     */
+    protected $header = null;
+
+    /**
+     * @var array The data set
+     */
+    protected $dataSet = array();
+
+    /**
+     * @var string The rendered result
+     */
+    protected $result = null;
+
+    /**
+     * @param \Twig_Environment $twig
+     * @param array $options
+     */
+    public function __construct(\Twig_Environment $twig, array $options = array())
+    {
+        parent::__construct($options);
+        $this->twig = $twig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'template' => '@SparksonDataExporterBundle/exporter/template.html.twig',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function begin()
+    {
+        parent::begin();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function writeHeaderRow(array $columnLabels)
+    {
+        $this->header = $columnLabels;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function writeRecordRow(array $columnLabels, array $record)
+    {
+        $fields = array();
+
+        /** @var Column $column */
+        foreach ($columnLabels as $key => $columnLabel) {
+            $fields[] = $record[$key];
+        }
+
+        $this->dataSet[] = $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function end()
+    {
+        $this->result = $this->twig->render($this->options['template'], array(
+            'header' => $this->header,
+            'data_set' => $this->dataSet,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+}
---
+++ Tests/Exporter/Output/TwigTemplateOutputAdapterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sparkson\DataExporterBundle\Tests\Exporter\Output;
+
+use Sparkson\DataExporterBundle\Exporter\Column\Column;
+use Sparkson\DataExporterBundle\Exporter\Column\ColumnSet;
+use Sparkson\DataExporterBundle\Exporter\Core\Type\StringType;
+use Sparkson\DataExporterBundle\Exporter\Exporter;
+use Sparkson\DataExporterBundle\Exporter\Output\TwigTemplateOutputAdapter;
+
+class TwigTemplateOutputAdapterTest extends \PHPUnit_Framework_TestCase {
+
+    private $dataSet = array(
+        array('firstName' => 'Foo', 'lastName' => 'Chan'),
+        array('firstName' => 'Bar', 'lastName' => 'Wong'),
+    );
+
+    public function testSimpleStructure()
+    {
+        $loader = new \Twig_Loader_Filesystem(__DIR__.'/../../../Resources/view/exporter');
+        $twig = new \Twig_Environment($loader, array());
+
+        $columns = new ColumnSet();
+        $columns->addChild(new Column('firstName', new StringType(), array('property_path' => '[firstName]')));
+        $columns->addChild(new Column('lastName', new StringType(), array('property_path' => '[lastName]')));
+
+        $exporter = new Exporter();
+        $exporter
+            ->setColumns($columns)
+            ->setOutput(new TwigTemplateOutputAdapter($twig, ['template' => 'template.html.twig']))
+            ->setDataSet($this->dataSet)
+            ->execute();
+
+        $result = $exporter->getResult();
+        $this->assertEquals('<table><thead><tr><th>First Name</th><th>Last Name</th></tr></thead><tbody><tr><td>Foo</td><td>Chan</td></tr><tr><td>Bar</td><td>Wong</td></tr></tbody></table>', $result);
+    }
+
+}
